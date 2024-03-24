16481ccfc55dcb71650f53301986fc10b1577582
Fernanda Villagran <villagranferu@gmail.com>
2024-03-24 19:11:09 -0300

    se agrega recuperar contraseña

--- Script/script_login.js
+++ Script/script_login.js
@@ -15,3 +15,36 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+// recuperar contraseña
+document.addEventListener('DOMContentLoaded', function() {
+    const forgotPasswordLink = document.getElementById('forgot-password-link');
+    const forgotPasswordModal = document.getElementById('forgot-password-modal');
+    const closeModal = document.querySelector('.close');
+    const sendResetEmailBtn = document.getElementById('send-reset-email');
+    const loginForm = document.getElementById('login-form');
+    
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    forgotPasswordLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      forgotPasswordModal.style.display = 'block';
+    });
+  
+    closeModal.addEventListener('click', function() {
+      forgotPasswordModal.style.display = 'none';
+    });
+  
+    sendResetEmailBtn.addEventListener('click', function(event) {
+      event.preventDefault();
+ 
+      const email = document.getElementById('forgot-email').value;
+      alert('Correo de recuperación enviado a: ' + email);
+      forgotPasswordModal.style.display = 'none';
+    });
+  
+    loginForm.addEventListener('submit', function(event) {
+      event.preventDefault();
+     alert('Formulario de inicio de sesión enviado');
+    });
+  });
